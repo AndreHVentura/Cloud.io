1904330e5613ded25206c932d93bf6d30a92b5d0
valcir-jr <162353778+valcir-jr@users.noreply.github.com>
2025-04-07 17:54:01 -0300

    Adding /home: App.tsx

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Cadastro from "./pages/Cadastro";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 import GlobalStyle from "./styles/GlobalStyles";
 
 function App() {
   return (
     <BrowserRouter>
-          <GlobalStyle /> {/* Deve estar apenas aqui */}
+        <GlobalStyle /> 
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );

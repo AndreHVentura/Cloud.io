290fa84f00e8b830b24b21d4e525d506a79cd5c9
valcir-jr <162353778+valcir-jr@users.noreply.github.com>
2025-04-10 19:48:54 -0300

    Envolvendo Home c/ o MainLayout

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainLayout><Login /></MainLayout>} />
         <Route path="/cadastro" element={<MainLayout><Cadastro /></MainLayout>} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<MainLayout><Home /></MainLayout>} />
       </Routes>
     </BrowserRouter>
   );

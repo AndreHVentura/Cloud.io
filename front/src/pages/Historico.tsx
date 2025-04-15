be5e561b61f6b3a367646a87a1c11dbaac8b96da
EduardoBrito2 <eduardo.brito7@fatec.sp.gov.br>
2025-04-15 15:49:31 -0300

    layout tela de historico

--- front/src/pages/Historico.tsx
+++ front/src/pages/Historico.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import NavBar from "../components/Navbar";
 import Topbar from "../components/Topbar";
+import styled from "styled-components";
+import { ButtonGroup, Container, StyledTable } from "../styles/historico";
 
 export default function Historico(){
     const [isActive, setIsActive] = useState<string>("close");
@@ -13,23 +15,64 @@ export default function Historico(){
         <Topbar helper={toggle} />
         {/* <>Graficos com registros meterologicos do dia ,com filtro para registros passados</> */}
         <br></br>
+        <Container>
+      <ButtonGroup>
         <button id="e1">Estação 1</button>
         <button id="e2">Estação 2</button>
         <button id="e3">Estação 3</button>
         <button id="filter">Filtro</button>
-        <div id="Date">Date</div>
-        <div id="Time">Time</div>
-        <div id="Temp_C">Temp_C</div>
-        <div id="Hum_%">Hum_%</div>
-        <div id="Press_Bar">Press_Bar</div>
-        <div id="TempCabine_C">TempCabine_C</div>
-        <div id="Charge">Charge</div>
-        <div id="SR_Wm2">SR_Wm2</div>
-        <div id="WindPeak_ms">WindPeak_ms</div>
-        <div id="WindSpeed_Avg">WindSpeed_Avg</div>
-        <div id="WindDir_Inst">WindDir_Inst</div>
-        <div id="WindDir_Avg">WindDir_Avg</div>
-        <div id="Wave_height">Wave_height</div>
+      </ButtonGroup>
+
+      <StyledTable>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Temp_C</th>
+            <th>Hum_%</th>
+            <th>Press_Bar</th>
+            <th>TempCabine_C</th>
+            <th>Charge</th>
+            <th>SR_Wm2</th>
+            <th>WindPeak_ms</th>
+            <th>WindSpeed_Avg</th>
+            <th>WindDir_Inst</th>
+            <th>WindDir_Avg</th>
+            <th>Wave_height</th>
+            <th>Data</th>
+        {/* <th>Hora</th>
+            <th>Temperatura (°C)</th>
+            <th>Umidade (%)</th>
+            <th>Pressão (Bar)</th>
+            <th>Temp. da Cabine (°C)</th>
+            <th>Carga</th>
+            <th>Radiação Solar (W/m²)</th>
+            <th>Pico de Vento (m/s)</th>
+            <th>Vel. Média do Vento (m/s)</th>
+            <th>Dir. Inst. do Vento</th>
+            <th>Dir. Média do Vento</th>
+            <th>Altura da Onda</th> */}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {/* <td id="Date"></td>
+            <td id="Time"></td>
+            <td id="Temp_C"></td>
+            <td id="Hum_%"></td>
+            <td id="Press_Bar"></td>
+            <td id="TempCabine_C"></td>
+            <td id="Charge"></td>
+            <td id="SR_Wm2"></td>
+            <td id="WindPeak_ms"></td>
+            <td id="WindSpeed_Avg"></td>
+            <td id="WindDir_Inst"></td>
+            <td id="WindDir_Avg"></td>
+            <td id="Wave_height"></td> */}
+          </tr>
+        </tbody>
+      </StyledTable>
+    </Container>
         </>
     )
 }

7fb2a77a193af18233c0a3b32e433079d1d41a25
BrunoHenrique258 <162308934+BrunoHenrique258@users.noreply.github.com>
2025-04-29 21:43:13 -0300

    Update HomeContent.tsx

--- front/src/components/perfil/HomeContent.tsx
+++ front/src/components/perfil/HomeContent.tsx
@@ -1,34 +1,25 @@
 import styled from "styled-components";
 import WeatherDashboard from "./WeatherDashboard";
-<<<<<<< HEAD
 import TempWidget from "../widgets/TempWidget";
 import HumWidget from "../widgets/HumWidget";
 import PressWidget from "../widgets/PressWidget";
 import TempCabineWidget from "../widgets/TempCabineWidget";
 import ChargeWidget from "../widgets/ChargeWidget";
 import SolarRadiationWidget from "../widgets/SolarRadiationWidget";
-import ReservoirLevelChart from "../graficos/ReservoirlevelChart";
-=======
-// import TempWidget from "../widgets/TempWidget";
-// import HumWidget from "../widgets/HumWidget";
-// import PressWidget from "../widgets/PressWidget";
-// import TempCabineWidget from "../widgets/TempCabineWidget";
-// import ChargeWidget from "../widgets/ChargeWidget";
-// import SolarRadiationWidget from "../widgets/SolarRadiationWidget";
->>>>>>> 9d755b09d16a8bc666b016bfa4a82ab693e40ab4
+
 
 export default function HomeContent() {
     return(
       <Main>
       <MainDiv>
-        {/* <WidgetsContainer>
+        <WidgetsContainer>
           <TempWidget />
           <HumWidget />
           <PressWidget />
           <TempCabineWidget />
           <ChargeWidget />
           <SolarRadiationWidget />
-        </WidgetsContainer> */}
+        </WidgetsContainer>
         <WeatherDashboard />
         {/* <ReservoirLevelChart/> */}
       </MainDiv>

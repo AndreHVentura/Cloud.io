4d8504a96016a376f4090872ea5892bef60896cb
valcir-jr <caralegal124@gmail.com>
2025-05-06 19:51:09 -0300

    Movendo a navbar p/ um componente

---
+++ front/src/components/perfil/NavbarPI.tsx
@@ -0,0 +1,92 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import ImgLogo from "../../logo/icone-nuvem.png"
+
+export default function NavbarPI() {
+  return (
+    <Navbar>
+      <img src={ImgLogo} alt="logo" width={"45rem"} height={"45rem"}/>
+      <NavGroup>
+        <NavLinks>
+          <PagISNavLink to="/grafic">Gráficos</PagISNavLink>
+          <PagISNavLink to="/alert">Alertas</PagISNavLink>
+          <PagISNavLink to="/">Clima</PagISNavLink>
+        </NavLinks>
+
+        <AuthButtons>
+          <BotaoLogin to="/login">Login</BotaoLogin>
+          <BotaoCadastro to="/cadastro">Cadastrar</BotaoCadastro>
+        </AuthButtons>
+      </NavGroup>
+    </Navbar>
+  );
+};
+
+const Navbar = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 4rem;
+  width: 100%;
+  height: 4rem;
+`;
+
+const NavGroup = styled.div`
+  display: flex;
+  gap: 2rem;
+  align-items: center;
+`;
+
+const NavLinks = styled.div`
+  display: flex;
+  gap: 2rem;
+  align-items: center;
+`;
+
+const PagISNavLink = styled(NavLink)`
+  text-decoration: none;
+  color: white;
+  font-weight: 500;
+  transition: 0.3s;
+
+  &:hover {
+    color: #8b5cf6;
+  }
+`;
+
+const AuthButtons = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+
+const BotaoLogin = styled(NavLink)`
+  background: transparent;
+  border: none;
+  color: white;
+  font-size: 1rem;
+  cursor: pointer;
+  text-decoration: none;
+  padding-top: 8px;
+  transition: 0.3s;
+
+  &:hover {
+    color: #8b5cf6
+  }
+`;
+
+const BotaoCadastro = styled(NavLink)`
+  background: transparent;
+  border: 1px solid white;
+  padding: 0.5rem 1.2rem;
+  border-radius: 6px;
+  color: white;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: background 0.3s;
+  text-decoration: none;
+
+  &:hover {
+    background: white;
+    color: #0e0e1a;
+  }
+`;
--- front/src/pages/Pagina_Inicial.tsx
+++ front/src/pages/Pagina_Inicial.tsx
@@ -1,29 +1,14 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import ImgFundo from "../logo/nuvens.jpg"
-import ImgLogo from "../logo/icone-nuvem.png"
+import NavbarPI from "../components/perfil/NavbarPI";
 
 const HomePage = () => {
   return (
     <Container>
       <Hero>
-        <Navbar>
-          <img src={ImgLogo} alt="logo" width={"45rem"} height={"45rem"}/>
-          <NavGroup>
-            <NavLinks>
-              <PagISNavLink to="/grafic">Gráficos</PagISNavLink>
-              <PagISNavLink to="/alert">Alertas</PagISNavLink>
-              <PagISNavLink to="/">Clima</PagISNavLink>
-            </NavLinks>
-  
-            <AuthButtons>
-              <BotaoLogin to="/login">Login</BotaoLogin>
-              <BotaoCadastro to="/cadastro">Cadastrar</BotaoCadastro>
-            </AuthButtons>
-          </NavGroup>
-        </Navbar>
+        <NavbarPI />
 
-      
         <HeroText>
           <Title>Cloud.IO</Title>
           <Subtitle>Plataforma para monitorar o clima.</Subtitle>
@@ -66,75 +51,6 @@ const Container = styled.div`
   color: white;
   min-height: 100vh;
   font-family: "Poppins", sans-serif;
-`;
-
-const Navbar = styled.nav`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 2rem 4rem;
-  width: 100%;
-  height: 4rem;
-`;
-
-const NavGroup = styled.div`
-  display: flex;
-  gap: 2rem;
-  align-items: center;
-`;
-
-const AuthButtons = styled.div`
-  display: flex;
-  gap: 1rem;
-`;
-
-const BotaoLogin = styled(NavLink)`
-  background: transparent;
-  border: none;
-  color: white;
-  font-size: 1rem;
-  cursor: pointer;
-  text-decoration: none;
-  padding-top: 8px;
-  transition: 0.3s;
-
-  &:hover {
-    color: #8b5cf6
-  }
-`;
-
-const BotaoCadastro = styled(NavLink)`
-  background: transparent;
-  border: 1px solid white;
-  padding: 0.5rem 1.2rem;
-  border-radius: 6px;
-  color: white;
-  font-size: 1rem;
-  cursor: pointer;
-  transition: background 0.3s;
-  text-decoration: none;
-
-  &:hover {
-    background: white;
-    color: #0e0e1a;
-  }
-`;
-
-const NavLinks = styled.div`
-  display: flex;
-  gap: 2rem;
-  align-items: center;
-`;
-
-const PagISNavLink = styled(NavLink)`
-  text-decoration: none;
-  color: white;
-  font-weight: 500;
-  transition: 0.3s;
-
-  &:hover {
-    color: #8b5cf6;
-  }
 `;
 
 const Hero = styled.section`

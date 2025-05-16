32f00743176bfa0955250dab3e62e7c3517683a1
EduardoBrito2 <eduardo.brito7@fatec.sp.gov.br>
2025-05-15 21:51:11 -0300

    excel

--- front/src/pages/Historico.tsx
+++ front/src/pages/Historico.tsx
@@ -4,6 +4,7 @@ import Topbar from "../components/perfil/Topbar";
 import { ButtonGroup, Container, StyledTable } from "../styles/historico";
 import { PDFDownloadLink } from "@react-pdf/renderer";
 import MyDocument from "../styles/pdf";
+import exportToExcel from "../styles/excel";
 
 export default function Historico() {
   const [isNavOpen, setIsNavOpen] = useState(false);
@@ -14,39 +15,54 @@ export default function Historico() {
     // Adicione mais dados conforme necessário
   ];
 
-    
-    return(
-        <>
-         <NavBar isOpen={isNavOpen} />
-         <Topbar helper={toggleNav} isNavOpen={isNavOpen} />
-        <br></br>
-        <Container>
-      <ButtonGroup>
-        <button id="e1">Estação 1</button>
-        <button id="e2">Estação 2</button>
-        <button id="e3">Estação 3</button>
-        <button id="filter">Filtro</button>
-      </ButtonGroup>
+  // const exportToExcel = () => {
+  //   const ws = XLSX.utils.table_to_sheet(document.getElementById('tabela') as HTMLTableElement);
+  //   const wb = XLSX.utils.book_new();
+  //   XLSX.utils.book_append_sheet(wb, ws, 'Tabela de dados');
+  //   const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+  //   const file = new Blob([excelBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+  //   const url = window.URL.createObjectURL(file);
 
-      <StyledTable>
-        <thead>
-          <tr>
-            <th>Data</th>
-            <th>Hora</th>
-            <th>Temperatura (°C)</th>
-            <th>Umidade (%)</th>
-            <th>Pressão (Bar)</th>
-            <th>Temp. da Cabine (°C)</th>
-            <th>Carga</th>
-            <th>Radiação Solar (W/m²)</th>
-            <th>Pico de Vento (m/s)</th>
-            <th>Vel. Média do Vento (m/s)</th>
-            <th>Dir. Inst. do Vento</th>
-            <th>Dir. Média do Vento</th>
-            <th>Altura da Onda</th>
-          </tr>
-        </thead>
-        <tbody>
+  //   const link = document.createElement('a');
+  //   link.href = url;
+  //   link.download = 'dados_meterologicos.xlsx'; // Nome do arquivo
+
+  //   link.click(); // Dispara o download
+  //   window.URL.revokeObjectURL(url); // Libera o objeto URL após o uso
+  // };
+
+  return (
+    <>
+      <NavBar isOpen={isNavOpen} />
+      <Topbar helper={toggleNav} isNavOpen={isNavOpen} />
+      <br></br>
+      <Container>
+        <ButtonGroup>
+          <button id="e1">Estação 1</button>
+          <button id="e2">Estação 2</button>
+          <button id="e3">Estação 3</button>
+          <button id="filter">Filtro</button>
+        </ButtonGroup>
+
+        <StyledTable id="tabela">
+          <thead>
+            <tr>
+              <th>Data</th>
+              <th>Hora</th>
+              <th>Temperatura (°C)</th>
+              <th>Umidade (%)</th>
+              <th>Pressão (Bar)</th>
+              <th>Temp. da Cabine (°C)</th>
+              <th>Carga</th>
+              <th>Radiação Solar (W/m²)</th>
+              <th>Pico de Vento (m/s)</th>
+              <th>Vel. Média do Vento (m/s)</th>
+              <th>Dir. Inst. do Vento</th>
+              <th>Dir. Média do Vento</th>
+              <th>Altura da Onda</th>
+            </tr>
+          </thead>
+          <tbody>
             {tableData.map((row, index) => (
               <tr key={index}>
                 <td>{row.date}</td>
@@ -65,11 +81,32 @@ export default function Historico() {
               </tr>
             ))}
           </tbody>
-      </StyledTable>
-      <PDFDownloadLink document={<MyDocument />} fileName="relatorio_meteológico.pdf">
-          {({ loading }) => (loading ? 'Carregando PDF...' : 'Baixar Relatório em PDF')}
-        </PDFDownloadLink>
-    </Container>
-        </>
-    )
+        </StyledTable>
+
+        <div style={{ marginTop: '20px' }}>
+          {/* Link para baixar o PDF */}
+          <PDFDownloadLink document={<MyDocument />} fileName="relatorio_meteológico.pdf">
+            {({ loading }) => (loading ? 'Carregando PDF...' : 'Baixar Relatório em PDF')}
+          </PDFDownloadLink>
+
+          {/* Botão para exportar para Excel */}
+          <button
+            onClick={exportToExcel}
+            style={{
+              background: 'none',
+              color: 'blue',
+              border: 'none',
+              textDecoration: 'underline',
+              cursor: 'pointer',
+              marginLeft: '20px',
+              padding: 0,
+              fontSize: '1rem'
+            }}
+          >
+            Exportar para Excel
+          </button>
+        </div>
+      </Container>
+    </>
+  );
 }
